f18857ad2422cd29693342a73b7ee876017c778c
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-20 20:11:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path=="/items/<ITEM_NAME>"
       item_name = req.params["item"]
       if @@items.contains?(item_name)
         resp.write "Item not found"
