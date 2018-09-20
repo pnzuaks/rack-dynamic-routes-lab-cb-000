4d1889dd12034fde4a0f8d576942d5b6295d5716
Patricia Nykaza <pnykaza@calstatela.edu>
2018-09-20 19:40:17 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/<ITEM NAME>)
-      resp.write "You requested the songs"
+      resp.write item.price
     else
       resp.write "Route not found"
       resp.status = 404
